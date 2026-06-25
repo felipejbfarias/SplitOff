04ccb5becfebbd5b6d6bc1cacdf17477c7422e76
Felipe Farias <felipejbfarias@hotmail.com>
2026-06-25 15:11:27 -0300

    Initial Commit

---
+++ SplitOff_project/SplitOff_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitOff_projectApp.swift
+//  SplitOff_project
+//
+//  Created by Felipe José Batista Farias on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitOff_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
