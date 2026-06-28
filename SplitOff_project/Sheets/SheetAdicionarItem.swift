52b21988e6ed2a6ee5c623cb309b0234af2e9fe9
Felipe Farias <felipejbfarias@hotmail.com>
2026-06-28 16:19:24 -0300

    Add SheetAdicionarItem and SheetEditarCardapio views

---
+++ SplitOff_project/Sheets/SheetAdicionarItem.swift
@@ -0,0 +1,18 @@
+//
+//  SheetAdicionarItem.swift
+//  SplitOff_project
+//
+//  Created by Felipe José Batista Farias on 6/28/26.
+//
+
+import SwiftUI
+
+struct SheetAdicionarItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetAdicionarItem()
+}
